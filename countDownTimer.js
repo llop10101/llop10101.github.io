9334709b814d20daeed220a695fa7df9d7c00f51
Llop10101 <joel.cano.puigvert@gmail.com>
2023-06-08 14:33:19 +0200

    idk

--- countDownTimer.js
+++ countDownTimer.js
@@ -2,10 +2,10 @@ var countDownDate = new Date("Dec 25, 2023 18:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var timeDiff = countDownDate - now;
-    var days = Math.floor(timeDiff/(1000*60*60*24));
-    var hours = Math.floor((timeDiff%(1000*60*60*24))/(1000*60*60));
-    var minutes = Math.floor((timeDiff%(1000*60*60*24))/(1000*60));
-    var seconds = Math.floor((timeDiff%(1000*60*60*24))/1000);
+    var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
     document.getElementById("temps").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     if(timeDiff<0){
         clearInterval(x);
