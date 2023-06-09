2d1011b5b3674db76f72f70ce4a39d4eabdc920a
llop10101 <joel.cano.puigvert@gmail.com>
2023-06-10 00:57:29 +0200

    calendari

--- calendarEvent.js
+++ calendarEvent.js
@@ -1,18 +1,17 @@
 var events = [
-    { dia: 7, hora: '15:30' },
-    { dia: 12, hora: '18:00' },
-    { dia: 20, hora: '11:00' },
-    { dia: 25, hora: '14:45' }
+    { grupo: "Pastors", hora: '15:30' },
+    { grupo: "Àngels", hora: '18:00' },
+    { grupo: "Dimonis", hora: '11:00' },
+    { grupo: "Personatges", hora: '14:45' }
   ];
-  
+
   var eventTableBody = document.getElementById('event-table-body');
-  
+
   for (var i = 0; i < events.length; i++) {
-    var eve = events[i];
+    var eventItem = events[i];
     var row = document.createElement('tr');
-    row.innerHTML = '<td>' + eve.dia + '</td>' +
-                    '<td>' + eve.dia + '/06/2023</td>' +
-                    '<td>' + eve.hora + '</td>';
+    row.innerHTML = '<td>' + eventItem.grupo + '</td>' +
+                    '<td>' + (i + 1) + '/06/2023</td>' +
+                    '<td>' + eventItem.hora + '</td>';
     eventTableBody.appendChild(row);
   }
-  
