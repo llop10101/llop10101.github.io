abea1e4e25cbed9916c9156fe8325bb8c5ab100b
llop10101 <joel.cano.puigvert@gmail.com>
2023-06-10 01:44:08 +0200

    prova fotos grid

---
+++ gridScript.js
@@ -0,0 +1,8 @@
+// Optional: Add interactivity to the grid (e.g., opening images in a lightbox)
+const gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    // Handle the click event, e.g., open the image in a lightbox
+  });
+});
