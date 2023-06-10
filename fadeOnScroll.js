76ca0dd1e2ec7b25d1c294e3fa0cfc3c2c97a129
llop10101 <joel.cano.puigvert@gmail.com>
2023-06-10 12:32:50 +0200

    scripts on toquen?

--- fadeOnScroll.js
+++ fadeOnScroll.js
@@ -1,15 +1,10 @@
-window.addEventListener("scroll", function() {
-    var textOverlay = document.getElementById("text-overlay");
-    var flipCardContainer = document.getElementById("flip-card-container");
-    var textOverlayHeight = textOverlay.offsetHeight;
-    var scrollPosition = window.pageYOffset;
-  
-    if (scrollPosition > textOverlayHeight) {
-      textOverlay.classList.add("fade");
-      flipCardContainer.classList.add("show");
-    } else {
-      textOverlay.classList.remove("fade");
-      flipCardContainer.classList.remove("show");
-    }
-  });
-  
+window.addEventListener('scroll', function() {
+  var imageContainer = document.getElementById('image-container');
+  var scrollPosition = window.scrollY || window.pageYOffset;
+
+  if (scrollPosition > 0) {
+      imageContainer.classList.add('fade');
+  } else {
+      imageContainer.classList.remove('fade');
+  }
+});
--- footerBottom.js
+++ footerBottom.js
@@ -1,10 +1,14 @@
-window.addEventListener('scroll', function() {
-var footer = document.getElementById('footer');
-var threshold = document.documentElement.scrollHeight - window.innerHeight;
-
-if (window.scrollY > threshold) {
-    footer.classList.remove('hidden');
-} else {
-    footer.classList.add('hidden');
-}
-});
+window.addEventListener("scroll", function() {
+    var textOverlay = document.getElementById("text-overlay");
+    var flipCardContainer = document.getElementById("flip-card-container");
+    var textOverlayHeight = textOverlay.offsetHeight;
+    var scrollPosition = window.pageYOffset;
+  
+    if (scrollPosition > textOverlayHeight) {
+      textOverlay.classList.add("fade");
+      flipCardContainer.classList.add("show");
+    } else {
+      textOverlay.classList.remove("fade");
+      flipCardContainer.classList.remove("show");
+    }
+  });
