6bff61e1bf3f5e8b219d0b0d4d198062f3b633b5
llop10101 <joel.cano.puigvert@gmail.com>
2023-06-10 12:53:48 +0200

    canvis per fer desapareixer l'overlay

--- fadeOnScroll.js
+++ fadeOnScroll.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', function() {
   var imageContainer = document.getElementById('image-container');
+  var textOverlay = document.getElementById('text-overlay');
   var scrollPosition = window.scrollY || window.pageYOffset;
 
   if (scrollPosition > 0) {
       imageContainer.classList.add('fade');
+      textOverlay.classList.add('fade');
   } else {
       imageContainer.classList.remove('fade');
+      textOverlay.classList.remove('fade');
   }
 });
